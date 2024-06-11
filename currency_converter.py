e14f7fd16e41ce7944f3822af17386c80f7d0659
Atul Venkatesan <atulvenky@gmail.com>
2024-06-11 15:41:00 +0530

    commit 2

--- currency_converter.py
+++ currency_converter.py
@@ -6,22 +6,33 @@ from forex_python.converter import CurrencyRates
 # Force the use of decimal
 c = CurrencyRates(force_decimal=True)
 
+
+def enter_data():
+    #amount = money_entry.get()
+    #start = curr_start_combobox.get()
+    #end = curr_end_combobox.get()
+    #output = c.convert(start, end, amount)
+    #print("Starting Amount: ", amount, " Initial Currency: ", start, " End Currency: ", end, "Converted Amount: ", output)
+    print(c.get_rates('USD'))
+
 root = tk.Tk()
 root.title("Currency Converter")
 
 money_label = tk.Label(root, text="Enter the monetary amount to be converted")
 money_entry = tk.Entry(root)
 curr_label_start = tk.Label(root, text="Choose your starting currency")
-curr_start_combobox = ttk.Combobox(root, values=[" ", "USD", "IDR", "BGN", "ILS", "GBP", "DKK", "CAD", "JPY", "HUF", "RON", "EUR", "HKD", "AUD", "CNY", "PHP"])
+curr_start_combobox = ttk.Combobox(root, values=[" ", "USD", "INR", "BGN", "ILS", "GBP", "DKK", "CAD", "JPY", "HUF", "RON", "EUR", "HKD", "AUD", "CNY", "PHP"])
 curr_label_end = tk.Label(root, text="Choose the currency you wish to convert to")
 curr_end_combobox = ttk.Combobox(root, values=[" ", "USD", "IDR", "BGN", "ILS", "GBP", "DKK", "CAD", "JPY", "HUF", "RON", "EUR", "HKD", "AUD", "CNY", "PHP"])
+enter_button = tk.Button(root, text="Convert", command= enter_data)
 
 
-money_label.pack(padx=7, pady=7, side=tk.LEFT)
+money_label.pack(padx=5, pady=5, side=tk.LEFT)
 money_entry.pack(padx=5, pady=5, side=tk.LEFT)
 curr_label_start.pack(padx=5, pady=5, side=tk.LEFT)
 curr_start_combobox.pack(padx=5, pady=5, side=tk.LEFT)
 curr_label_end.pack(padx=5, pady=5, side=tk.LEFT)
 curr_end_combobox.pack(padx=5, pady=5, side=tk.LEFT)
+enter_button.pack(padx=5, pady=5, side=tk.LEFT)
 
 root.mainloop()
